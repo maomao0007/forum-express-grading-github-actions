0bcbc51d51522f4422f76d420787ad9bb521aa47
maomao0007 <skylifepic07@gmail.com>
2024-06-28 21:09:20 +0800

    feat: user model add is_admin

---
+++ middleware/auth.js
@@ -0,0 +1,32 @@
+const { ensureAuthenticated, getUser } = require("../helpers/auth-helpers");
+const authenticated = (req, res, next) => {
+  // 檢查使用者是否已經通過身份驗證
+  // if (req.isAuthenticated)
+  if (ensureAuthenticated(req)) {
+    // 如果已經通過身份驗證，調用 next() 繼續處理請求
+    return next();
+  }
+  // 如果沒有通過身份驗證，則重定向到登入頁面 '/signin'
+  res.redirect("/signin");
+};
+
+const authenticatedAdmin = (req, res, next) => {
+  // 檢查使用者是否已經通過身份驗證
+  // if (req.isAuthenticated)
+  if (ensureAuthenticated(req)) {
+    // 如果已經通過身份驗證，進一步檢查使用者是否具有管理員權限
+    if (getUser(req).isAdmin) {
+      // 如果是管理員，則調用 next() 繼續處理請求
+      return next();
+    }
+    // 如果不是管理員，則重定向到主頁面 '/'
+    res.redirect("/");
+  } else {
+    // 如果沒有通過身份驗證，則重定向到登入頁面 '/signin'
+    res.redirect("/signin");
+  }
+};
+module.exports = {
+  authenticated,
+  authenticatedAdmin,
+};
