94e71add76def3099982f967d2e523393f31a93f
maomao0007 <skylifepic07@gmail.com>
2024-07-04 16:53:04 +0800

    Add the restaurant dashboard page and implement the view count feature.

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -26,6 +26,24 @@ const restaurantController = {
         return res.render("restaurant", { restaurant });
       })
       .catch(err => next(err))
+  },
+  getDashboard: (req, res, next) => {
+    let viewCount = 0; // 初始化瀏覽次數
+    return Restaurant.findByPk(req.params.id, {
+      include: Category,
+      nest: true,
+      raw: true,
+    })
+      .then((restaurant) => {
+        if (!restaurant) throw new error("This restaurant didn't exist.");
+
+        viewCount++;
+        return res.render("dashboard", {
+          restaurant,
+          viewCount
+        });
+      })
+      .catch((err) => next(err));
   }
-}
+} 
 module.exports = restaurantController;
