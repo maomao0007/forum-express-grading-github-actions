ca8cd239f1dfaf7a6eadf675b6803fa471d62d8b
maomao0007 <skylifepic07@gmail.com>
2024-07-06 00:30:23 +0800

    feat: add delete comment for admin

--- routes/index.js
+++ routes/index.js
@@ -17,7 +17,8 @@ failureFlash: true,}),userController.signIn); // 注意是 post
 router.get("/logout", userController.logout);
 router.get("/restaurants/:id", authenticated, restController.getRestaurant); 
 router.get("/restaurants/:id/dashboard", authenticated, restController.getDashboard);
-router.get("/restaurants", authenticated, restController.getRestaurants); 
+router.get("/restaurants", authenticated, restController.getRestaurants)\
+router.delete('/comments/:id', authenticatedAdmin, commentController.deleteComment); 
 router.post("/comments", authenticated, commentController.postComment); 
 router.use("/", (req, res) => res.redirect("/restaurants"));
 router.use("/", generalErrorHandler); 
