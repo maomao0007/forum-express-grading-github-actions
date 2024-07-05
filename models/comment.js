c033eed8504551f0933a9e3597d448d0d428dfb5
maomao0007 <skylifepic07@gmail.com>
2024-07-05 17:26:46 +0800

    feat: add comment model

---
+++ models/comment.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Comment extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Comment.belongsTO(models.Restaurant, { foreignkey: "restaurantId" });
+      Comment.belongsTO(models.User, { foreignkey: "userId" }); 
+    }
+  };
+  Comment.init({
+    text: DataTypes.STRING,
+    userId: DataTypes.INTEGER,
+    restaurantId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Comment',
+    underscored: true,
+  });
+  return Comment;
+};
